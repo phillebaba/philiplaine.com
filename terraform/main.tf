65d02632803673007e1cfee8df30e50984aeb0cf
Philip Laine <philip.laine@gmail.com>
2019-04-02 22:50:58 +0200

    Update modules and add tags to resources

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,14 @@
+locals {
+  tags = {
+    Project = "philiplaine.com"
+    Environment = "prod"
+  }
+}
+
 module "s3_static_website" {
   source = "github.com/phillebaba/terraform-modules/modules/s3-static-website"
 
+  tags = "${local.tags}"
   domain_name = "${var.domain_name}"
   index_document = "index.html"
   error_document = "404.html"
@@ -19,6 +27,7 @@ module "s3_static_website" {
 module "cloudfront_cdn" {
   source = "github.com/phillebaba/terraform-modules/modules/cloudfront-cdn"
 
+  tags = "${local.tags}"
   domain_name = "${var.domain_name}"
-  origin_domain_name = "${module.s3_static_website.s3_website_endpoint}"
+  origin_domain_name = "${module.s3_static_website.website_endpoint}"
 }
