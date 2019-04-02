58bc9a2def2b54b7b56f169900f26968bff58700
Philip Laine <philip.laine@gmail.com>
2019-04-02 22:51:47 +0200

    Add CloudWatch dashboard for S3 and CloudFront metrics

--- terraform/main.tf
+++ terraform/main.tf
@@ -31,3 +31,66 @@ module "cloudfront_cdn" {
   domain_name = "${var.domain_name}"
   origin_domain_name = "${module.s3_static_website.website_endpoint}"
 }
+
+resource "aws_cloudwatch_dashboard" "default" {
+  dashboard_name = "philiplaine-com"
+
+  dashboard_body = <<EOF
+  {
+  "start": "-P7D",
+    "widgets": [
+        {
+            "type": "metric",
+            "x": 0,
+            "y": 0,
+            "width": 24,
+            "height": 6,
+            "properties": {
+                "view": "timeSeries",
+                "stacked": false,
+                "metrics": [
+                    [ "AWS/S3", "NumberOfObjects", "StorageType", "AllStorageTypes", "BucketName", "${module.s3_static_website.bucket_name}", { "period": 86400 } ],
+                    [ ".", "BucketSizeBytes", ".", "StandardStorage", ".", ".", { "period": 86400 } ]
+                ],
+                "region": "eu-west-1",
+                "period": 300,
+                "title": "S3 Bucket Storage"
+            }
+        },
+        {
+            "type": "metric",
+            "x": 0,
+            "y": 6,
+            "width": 12,
+            "height": 9,
+            "properties": {
+                "view": "timeSeries",
+                "stacked": false,
+                "metrics": [
+                    [ "AWS/CloudFront", "Requests", "Region", "Global", "DistributionId", "${module.cloudfront_cdn.distribution_id}" ]
+                ],
+                "region": "us-east-1",
+                "title": "Total Requests",
+                "period": 300
+            }
+        },
+        {
+            "type": "metric",
+            "x": 12,
+            "y": 6,
+            "width": 12,
+            "height": 9,
+            "properties": {
+                "view": "timeSeries",
+                "stacked": false,
+                "metrics": [
+                    [ "AWS/CloudFront", "4xxErrorRate", "Region", "Global", "DistributionId", "${module.cloudfront_cdn.distribution_id}" ],
+                    [ ".", "5xxErrorRate", ".", ".", ".", "." ]
+                ],
+                "region": "us-east-1"
+            }
+        }
+    ]
+  }
+EOF
+}
